aed392586631632690fa59ef525273becd5eab44
Nishantbj <91536933+Nishantbj@users.noreply.github.com>
2022-02-26 16:09:02 +0530

    Add files via upload

--- boy.js
+++ boy.js
@@ -1,18 +1,47 @@
 class Boy {
-    constructor(x, y, w, h) {
-        var options = {
-            isStatic: true
-
-        }
-        this.body = Bodies.rectangle(x, y, w, h, options)
-        World.add(world, this.body)
-        this.width = w
-        this.height = h
-        this.image = boyStd
+    constructor(x, y) {
+      var options = {
+        isStatic: true
+      };
+      this.r = 30;
+      this.speed = 0.05;
+      this.body = Bodies.circle(x, y, this.r, options);
+      this.image = loadImage("./assets/boy4.png");
+     // this.animation = [this.image];
+      
+      
+      World.add(world, this.body);
     }
-    show() {
-        image(this.image,this.body.position.x,this.body.position.y, this.w, this.h)
-
-        
+  
+    animate() {
+      this.speed += 0.05;
     }
-}
+    shoot() {
+        var newAngle = boy.angle - 28;
+        newAngle = newAngle *(3.14/180)
+        var velocity = p5.Vector.fromAngle(newAngle);
+        velocity.mult(0.5);
+        Matter.Body.setStatic(this.body, false);
+        Matter.Body.setVelocity(this.body, {
+          x: velocity.x *(180/3.14), y: velocity.y * (180/3.14)});
+      }
+    
+      show() {
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        //var index = floor(this.speed % this.animation.length);
+    
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.r, this.r);
+        pop();
+    
+       
+    
+     
+      }
+    }
+    
+    
